a94d96033d6a777d62c65ee5ca619f334335d0f2
Binara Medawatta <binaramedawatta@gmail.com>
2019-11-02 13:26:33 +0530

    Initialised HotChocolate

---
+++ HotChocolate.cpp
@@ -0,0 +1,15 @@
+// Hot Chocolate vending machine
+
+int main(){
+	int value;
+	
+	while(1){
+		cout << "*******Main Menu*******" << endl;
+		cout << "1. Milo" << endl;
+		cout << "2. Coffee" << endl;
+		cout << "3. Tea" << endl;
+		cout << "**********END**********" << endl;
+		cout << "Enter your choice : " << endl;
+		cin <<
+	}
+}
