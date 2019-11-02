ccfe3af4f8bbb673ded1c56bd7450c882d51e957
Binara Medawatta <binaramedawatta@gmail.com>
2019-11-02 13:35:27 +0530

    Modified hot chocolate

--- HotChocolate.cpp
+++ HotChocolate.cpp
@@ -1,7 +1,10 @@
 // Hot Chocolate vending machine
+#include <iostream>
+
+using namespace std;
 
 int main(){
-	int value;
+	int value, choice, coin;
 	
 	while(1){
 		cout << "*******Main Menu*******" << endl;
@@ -9,7 +12,45 @@ int main(){
 		cout << "2. Coffee" << endl;
 		cout << "3. Tea" << endl;
 		cout << "**********END**********" << endl;
-		cout << "Enter your choice : " << endl;
-		cin <<
+		cout << "Enter your choice : ";
+		cin >> choice;
+		
+		switch(choice){
+			case 1 :
+				value=0;
+				coin=0;
+				cout << "Price : Rs 10" << endl;
+				while(value<10){
+					cout << 10-value << " coins needed" << endl;
+					cin >> coin;
+					value+=coin;
+				}
+				cout << "Enjoy your Milo" << endl;
+				break;
+			
+			case 2 :
+				value=0;
+				coin=0;
+				cout << "Price : Rs 20" << endl;
+				while(value<20){
+					cout << 20-value << " coins needed" << endl;
+					cin >> coin;
+					value+=coin;
+				}
+				cout << "Enjoy your Coffee" << endl;
+				break;
+				
+			case 3 :
+				value=0;
+				coin=0;
+				cout << "Price : Rs 15" << endl;
+				while(value<15){
+					cout << 15-value << " coins needed" << endl;
+					cin >> coin;
+					value+=coin;
+				}
+				cout << "Enjoy your Tea" << endl;
+				break;
+		}
 	}
 }
