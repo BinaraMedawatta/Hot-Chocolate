373e37051f990b5137a6dab29ba405eda6823720
Binara Medawatta <binaramedawatta@gmail.com>
2019-11-20 20:47:10 +0530

    Added insert coins function

--- HotChocolate.cpp
+++ HotChocolate.cpp
@@ -1,56 +1,65 @@
 // Hot Chocolate vending machine
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
+int insertCoins(int value, int price, string beverage){
+	int coin = 0;
+	while(value<price){
+		cout << price-value << " coins needed" << endl;
+		cout << "Insert coins(press 0 to wrapup) : ";
+		cin >> coin;
+		if(coin==0){
+			cout << "Bye" << endl;
+			return value;
+		}
+		value+=coin;
+	}
+	if(value>=price){
+		value -= price;
+		cout << "Enjoy your " << beverage << endl;
+	}
+	return value;
+}
+
 int main(){
-	int value, choice, coin;
+	int value=0, choice, coin=0;
 	
 	while(1){
 		cout << "*******Main Menu*******" << endl;
 		cout << "1. Milo" << endl;
 		cout << "2. Coffee" << endl;
 		cout << "3. Tea" << endl;
+		cout << "4. Check coin total" << endl;
+		cout << "5. Exit" << endl;
 		cout << "**********END**********" << endl;
 		cout << "Enter your choice : ";
 		cin >> choice;
 		
 		switch(choice){
 			case 1 :
-				value=0;
-				coin=0;
 				cout << "Price : Rs 10" << endl;
-				while(value<10){
-					cout << 10-value << " coins needed" << endl;
-					cin >> coin;
-					value+=coin;
-				}
-				cout << "Enjoy your Milo" << endl;
+				value = insertCoins(value, 10, "Milo");
 				break;
 			
 			case 2 :
-				value=0;
-				coin=0;
 				cout << "Price : Rs 20" << endl;
-				while(value<20){
-					cout << 20-value << " coins needed" << endl;
-					cin >> coin;
-					value+=coin;
-				}
-				cout << "Enjoy your Coffee" << endl;
+				value = insertCoins(value, 20, "Coffee");
 				break;
 				
 			case 3 :
-				value=0;
-				coin=0;
 				cout << "Price : Rs 15" << endl;
-				while(value<15){
-					cout << 15-value << " coins needed" << endl;
-					cin >> coin;
-					value+=coin;
-				}
-				cout << "Enjoy your Tea" << endl;
+				value = insertCoins(value, 15, "Tea");
 				break;
+			
+			case 4 : 
+				cout << value;
+				break;
+			
+			case 5 :
+				cout << "Good Bye";
+				exit(0);
 		}
 	}
 }
